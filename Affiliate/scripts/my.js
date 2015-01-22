e7eb0a82661064bb39c5aaeedd0a5d40a34d348f
WalkieWorld <Hao.Zhang@du.edu>
2015-01-22 12:06:56 -0700

    (1)Add new page, survivalfrog.html. (2)Fix page footer style.

--- Affiliate/scripts/my.js
+++ Affiliate/scripts/my.js
@@ -1,13 +1,20 @@
 $( document ).ready(function() {
 	
-	$('#myTab a').click(function (e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
-
 	$('#clickBankTab a').click(function (e) {
 		e.preventDefault();
 		$(this).tab('show');
 	});
+
+	$('#solarAirLanternTab a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$('#grenadeTab a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$('#scroll-fix-bar')
 
 });
