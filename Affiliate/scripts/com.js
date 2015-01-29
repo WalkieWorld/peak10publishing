b71e34cc0fb624ec330bf94f3e229088bfcf72d4
WalkieWorld <Hao.Zhang@du.edu>
2015-01-29 10:42:53 -0700

    (1)Create a new page as the Home page. (2)Complete the Advertise With Us page.

--- Affiliate/scripts/com.js
+++ Affiliate/scripts/com.js
@@ -58,21 +58,47 @@ $( document ).ready(function() {
 	// detect current size of window for using the right style
 	dynamicStyle();
 
+	$('#webFormSignUpShow').on('click', function(){
+
+		var tag = $('#show_field_Email');
+
+		if(verifySignUpForm(tag)){
+
+			$('#inf_field_Email').val($('#show_field_Email').val());
+			$('#inf_field_FirstName').val($('#show_field_FirstName').val());
+			$('#inf_field_LastName').val($('#show_field_LastName').val());
+			$('#inf_field_Phone1').val($('#show_field_Phone1').val());
+			$('#inf_field_clickbankop').val($('#show_custom_clickbankhop').val());
+			$('#inf_field_Nickname').val($('#show_field_Nickname').val());
+			
+			$('#signUpModal').submit();
+		}
+	});
 
 	/**
 	*	Verify the email address of the form, #signUpModel, when it submit.
 	*/
-	$('#signUpModal').submit(function(event){
+	var verifySignUpForm = function(tag){
 
-		var email = $('#inf_field_Email').val();
+		var email = tag.val();
 
 		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 		
 		if(email === "" || pattern.test(email) === false){
 
-			$('#inf_field_Email').addClass('text-danger');
+			tag.addClass('text-danger');
 			return false;
+		}else{
+
+			return true;
 		}
+	};
+
+
+	$('#signUpModal').submit(function(event){
+
+		var tag = $('#inf_field_Email');
+		return verifySignUpForm(tag);
 	});
 	
 	/**
